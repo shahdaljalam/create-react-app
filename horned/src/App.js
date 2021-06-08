6a71302010a67b71159f59051c0ee68a90a3d950
shahdaljalam <shahd.jm33@gmail.com>
2021-06-09 01:31:16 +0300

    add ls files

---
+++ horned/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React, { Component } from 'react'
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ horned/src/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import HornedBeast from './HornedBeast'
+
+
+export class main extends Component {
+    render() {
+        return (
+         <HornedBeast />
+        )
+    }
+}
+
+export default main
